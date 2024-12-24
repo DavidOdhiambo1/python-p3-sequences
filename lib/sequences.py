401e535b5cb95abb7c98627f1072c157ee6f7ae5
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-12-24 09:02:46 +0300

    first commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    for i in range(length):
+        if i == 0:
+            fibonacci_list.append(0)
+        elif i == 1:
+            fibonacci_list.append(1)
+        else:
+            fibonacci_list.append(fibonacci_list[i-1] + fibonacci_list[i-2])
+    print(fibonacci_list)
+   
